4f3512ab6e4189133056e3fcab943e1ac79b7d4d
mohamed <mohamed@mohamed-HP-ENVY>
2017-03-08 20:36:59 +0100

    3d commit , I commit in master, but modified in branch

--- TestJava/src/main/java/MyApplication.java
+++ TestJava/src/main/java/MyApplication.java
@@ -4,6 +4,7 @@ public class MyApplication {
 	public static void main(String[] args) {
 	//first version before push on remote rep
     //2cd commita after project has being pushed
+	//3d commit after create and swith to Branch_1
 		System.out.println("Hello wrold");
 
 	}
