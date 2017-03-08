f9343c58d8ac1db953215a8c57784b1b0c01bfd7
mohamed <mohamed@mohamed-HP-ENVY>
2017-03-08 20:07:10 +0100

    first commit before push

--- TestJava/src/main/java/MyApplication.java
+++ TestJava/src/main/java/MyApplication.java
@@ -2,6 +2,7 @@
 public class MyApplication {
 
 	public static void main(String[] args) {
+	//first version before push on remote rep
 		// TODO Auto-generated method stub
 		System.out.println("Hello wrold");
 
