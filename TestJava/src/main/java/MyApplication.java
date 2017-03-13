afcb310bbf1a5b839526773e619c8811a53f96e4
mohamed <mohamed@mohamed-HP-ENVY>
2017-03-13 22:18:18 +0100

    adding new file test and new lin test in java class

--- TestJava/src/main/java/MyApplication.java
+++ TestJava/src/main/java/MyApplication.java
@@ -4,6 +4,7 @@ public class MyApplication {
 	public static void main(String[] args) {
 	//first version before push on remote rep
     //2cd commita after project has being pushed
+	//adding test file in branch_1 (after checkout)
 		System.out.println("Hello wrold");
 
 	}
