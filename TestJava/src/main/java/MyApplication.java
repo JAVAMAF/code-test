92880b88bff09c94ef46510b680dbc3ce1ad3884
mohamed <mohamed@mohamed-HP-ENVY>
2017-11-17 11:53:09 +0100

    comit on local branch1 1st mopdification

--- TestJava/src/main/java/MyApplication.java
+++ TestJava/src/main/java/MyApplication.java
@@ -8,7 +8,7 @@ public class MyApplication {
 	//4d commoit directly in master on github
 		System.out.println("Hello wrold");
 		//1st modification on remote server
-
+		//1st modificzion in branch1 after creaton
 	}
 
 }
